6503e79d82a458bb90de834ed3aa25e5efd01f97
Sergey Sedov <sidslog@gmail.com>
2014-03-02 17:59:57 +0400

    Initial Commit

---
+++ WebServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WebServer
+//
+//  Created by sidslog on 02.03.14.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
